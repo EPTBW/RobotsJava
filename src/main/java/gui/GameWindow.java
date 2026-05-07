31fda22ed65174ad7ea9fd40a885df7860f12662
EPTBW <4urbanbro@mail.ru>
2026-05-07 23:42:33 +0500

    создан класс BaseInternalFrame для устранения дублирования кода для закрытия окон

--- src/main/java/gui/GameWindow.java
+++ src/main/java/gui/GameWindow.java
@@ -1,13 +1,9 @@
 package gui;
 
 import java.awt.BorderLayout;
-import javax.swing.JOptionPane;
-import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
-import javax.swing.event.InternalFrameAdapter;
-import javax.swing.event.InternalFrameEvent;
 
-public class GameWindow extends JInternalFrame
+public class GameWindow extends BaseInternalFrame
 {
     private final GameVisualizer m_visualizer;
     public GameWindow() 
@@ -18,27 +14,5 @@ public class GameWindow extends JInternalFrame
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-
-        addInternalFrameListener(new InternalFrameAdapter() {
-            @Override
-            public void internalFrameClosing(InternalFrameEvent e) {
-                confirmClose();
-            }
-        });
     }
-
-    private void confirmClose() {
-        Object[] options = {"Да", "Нет"};
-        int n = JOptionPane.showOptionDialog(this,
-                "Закрыть игровое поле?",
-                "Подтверждение",
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null, options, options[0]);
-        if (n == JOptionPane.YES_OPTION) {
-            dispose();
-        }
-    }
-
 }
